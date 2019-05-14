493c53d825de2b5d4904a67c9c1500bbaaf2e7da
Mahmoud Miri <sm_miri83@yahoo.com>
2019-05-14 01:03:29 +0100

    52

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
             {name: "Mark", age: 30},
             {name: "Sarah", age: 35}
         ],
-        otherState: "some value"
+        otherState: "some value",
+        showPersons: false
     };
 
     switchNameHandler = (newName) => {
@@ -34,7 +35,14 @@ class App extends Component {
 
     };
 
-  render() {
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({
+            showPersons: !doesShow
+        })
+    };
+
+    render() {
       const style = {
           backgroundColor: 'white',
           font: 'inherit',
@@ -47,20 +55,27 @@ class App extends Component {
           <div className="App">
               <h1>Hi. I'm a React app</h1>
               <p>This is working!</p>
-              <button style={style} onClick={() => this.switchNameHandler("Mahmoud")}>Switch Name</button>
-              <Person
-                  name={this.state.persons[0].name}
-                  age={this.state.persons[0].age}/>
-              <Person name={this.state.persons[1].name}
-                      age={this.state.persons[1].age}
-                      click={this.switchNameHandler.bind(this, "Paria")}
-                      changed={this.nameChangedHandler}
-              >My Hobbies: Racing</Person>
-              <Person name={this.state.persons[2].name}
-                      age={this.state.persons[2].age}/>
+              <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+              {/*wrapping the div inside {} allows us to use simple JS code; ternary operator here.*/}
+              {this.state.showPersons ?
+                  <div>
+                      <Person
+                          name={this.state.persons[0].name}
+                          age={this.state.persons[0].age}/>
+
+                      <Person name={this.state.persons[1].name}
+                              age={this.state.persons[1].age}
+                              click={this.switchNameHandler.bind(this, "Paria")}
+                              changed={this.nameChangedHandler}
+                      >My Hobbies: Racing</Person>
+
+                      <Person name={this.state.persons[2].name}
+                              age={this.state.persons[2].age}/>
+                </div> : null
+              }
           </div>
       );
-  }
+    }
 }
 
 export default App;
