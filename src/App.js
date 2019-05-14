b6eba542d1788895cdf2b809ff5101ecf621d248
Mahmoud Miri <sm_miri83@yahoo.com>
2019-05-14 22:34:00 +0100

    67

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
     state = {
@@ -58,7 +59,11 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }
         };
 
         let persons = null;
@@ -77,6 +82,10 @@ class App extends Component {
                 </div>
             );
             style.backgroundColor = 'red';
+            style[':hover'] = {
+                backgroundColor: 'salmon',
+                color: 'black'
+            }
         }
 
         const classes = [];
@@ -98,4 +107,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
