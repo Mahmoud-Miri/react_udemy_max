c473d7e3ea0a56da5312a6e02c982d0a109a1686
Mahmoud Miri <sm_miri83@yahoo.com>
2019-05-14 10:34:46 +0100

    53

--- src/App.js
+++ src/App.js
@@ -43,38 +43,43 @@ class App extends Component {
     };
 
     render() {
-      const style = {
-          backgroundColor: 'white',
-          font: 'inherit',
-          border: '1px solid blue',
-          padding: '8px',
-          cursor: 'pointer'
-      };
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
 
-      return (
-          <div className="App">
-              <h1>Hi. I'm a React app</h1>
-              <p>This is working!</p>
-              <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-              {/*wrapping the div inside {} allows us to use simple JS code; ternary operator here.*/}
-              {this.state.showPersons ?
-                  <div>
-                      <Person
-                          name={this.state.persons[0].name}
-                          age={this.state.persons[0].age}/>
+        let persons = null;
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}/>
 
-                      <Person name={this.state.persons[1].name}
-                              age={this.state.persons[1].age}
-                              click={this.switchNameHandler.bind(this, "Paria")}
-                              changed={this.nameChangedHandler}
-                      >My Hobbies: Racing</Person>
+                    <Person name={this.state.persons[1].name}
+                            age={this.state.persons[1].age}
+                            click={this.switchNameHandler.bind(this, "Paria")}
+                            changed={this.nameChangedHandler}
+                    >My Hobbies: Racing</Person>
 
-                      <Person name={this.state.persons[2].name}
-                              age={this.state.persons[2].age}/>
-                </div> : null
-              }
-          </div>
-      );
+                    <Person name={this.state.persons[2].name}
+                            age={this.state.persons[2].age}/>
+                </div>
+            );
+        }
+
+
+        return (
+            <div className="App">
+                <h1>Hi. I'm a React app</h1>
+                <p>This is working!</p>
+                <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+                {persons}
+            </div>
+        );
     }
 }
 
