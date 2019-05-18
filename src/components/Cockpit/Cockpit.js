6769144e29738d5efe217af86f9f4d0f08a53338
Mahmoud Miri <sm_miri83@yahoo.com>
2019-05-18 21:24:21 +0100

    93

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const cockpit = (props) => {
+    useEffect(() => {
+        console.log("Cockpit.js useEffect")
+        setTimeout(() => {
+            console.log("Data saved to cloud!");
+        }, 1000);
+        return (
+            () => {
+                console.log("Cockpit.js cleanup")
+            }
+        );
+    }, []);
+
+    //like useSate, you can have as many useEffect as you need
+
     return (
         <div>
             <h1>Hi. I'm a React app</h1>
